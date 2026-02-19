916307113ad3fca76cbcfb530922035d720c535c
Nifemi-Adewusi <92606720+Nifemi-Adewusi@users.noreply.github.com>
2026-02-19 02:42:35 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
+//import {
   AiFillGithub,
   AiOutlineTwitter,
   // AiFillInstagram,
