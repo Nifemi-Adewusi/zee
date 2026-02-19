047450078d2c727bcc0260afb9f2c503abef1980
Nifemi-Adewusi <92606720+Nifemi-Adewusi@users.noreply.github.com>
2026-02-19 02:43:17 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,13 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-//import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  // AiFillInstagram,
-  // AiFillTelegram,
-  // AiFillLinkedin,
-} from "react-icons/ai";
+
 // import { FaTelegramPlane } from "react-icons/fa";
 
 function Home2() {
